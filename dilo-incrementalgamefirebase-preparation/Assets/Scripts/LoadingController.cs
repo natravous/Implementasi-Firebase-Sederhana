982aaa052cea1a86a28c7c050d8a54143bf5f488
natravous <67213861+natravous@users.noreply.github.com>
2021-10-02 19:26:02 +0700

    Check Local

---
+++ dilo-incrementalgamefirebase-preparation/Assets/Scripts/LoadingController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingController : MonoBehaviour
+{
+    //[SerializeField] private Button _localButton;
+    //[SerializeField] private Button _cloudButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_localButton.onClick.AddListener(() =>
+        //{
+        //    SetButtonInteractable(false);
+        //    UserDataManager.LoadFromLocal();
+        //    SceneManager.LoadScene(1);
+        //});
+
+        //_cloudButton.onClick.AddListener(() =>
+        //{
+        //    SetButtonInteractable(false);
+        //    StartCoroutine(UserDataManager.LoadFromCloud(() => SceneManager.LoadScene(1)));
+        //});
+
+        // button didisable agar mencegah tidak terjadinya spam klik ketika
+        // proses onclick pada button sedang berjalan
+
+
+
+        UserDataManager.Load();
+        SceneManager.LoadScene(1);
+    }
+
+    // men-disable button agar tidak bisa ditekan
+    //private void SetButtonInteractable(bool interactable)
+    //{
+    //    _localButton.interactable = interactable;
+    //    _cloudButton.interactable = interactable;
+    //}
+}
---
+++ dilo-incrementalgamefirebase-preparation/Assets/Scripts/UserProgressData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] //menambahkan System.Serializable agar object nantinya dapat di-serialize menjadi string untuk disimpan 
+//dan kemudian di-deserialize kembali menjadi object untuk di-load.
+public class UserProgressData
+{
+    public double Gold = 0;
+    public List<int> ResourcesLevels = new List<int>();
+}
